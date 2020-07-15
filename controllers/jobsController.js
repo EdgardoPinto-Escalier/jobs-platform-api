7f1786d1ab146a905831db5fa5e02e2e324a0be4
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-07-15 16:07:13 +0200

    Created the controller method

---
+++ controllers/jobsController.js
@@ -0,0 +1,9 @@
+
+
+// Get all the jobs from /api/v1/jobs
+exports.getJobs = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: 'This route will display all jobs in the future'
+  });
+}
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/jobs', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'This route will display all jobs in the future'
-  });
-});
+// Import the jobs controller methods
+const { getJobs } = require('../controllers/jobsController');
+
+router.route('/jobs').get(getJobs);
 
 
 
