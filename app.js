de4a8c1c7208e03c21626e43eed83ea157a1d8d9
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-07-15 12:44:01 +0200

    Created the first basic route to get all jobs

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const dotenv = require('dotenv');
 // Setting config.env file variables
 dotenv.config({path : './config/config.env'})
 
+// Import routes here
+const jobs = require('./routes/jobs');
+
+
+app.use("/api/v1", jobs);
+
 
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${process.env.PORT}`);
+  console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 });
